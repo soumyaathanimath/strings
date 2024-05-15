00d0dda3ff54aa61521438658596f4d2ae780be1
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 11:38:29 +0530

    Strings

---
+++ src/strings/Strings.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package strings;
+
+/**
+ *
+ * @author dell1
+ */
+public class Strings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // TODO Auto-generated method stub
+		//String str1="Soumya.kiran.athanimath";
+		//System.out.println(str1.startsWith("Soumya"));
+		//System.out.println(str1.endsWith("athanimath"));
+		//System.out.println(str1.charAt(4));
+
+		//String str2="www.udemy.com";
+		//System.out.println(str1.indexOf("."));
+		//System.out.println(str2.lastIndexOf("."));
+	
+	String str1="Pyramid";
+	String str2="Pyramid";
+	
+	
+	System.out.println(str1==str2);
+	
+	
+	}
+	
+	
+
+}
+
+ 
